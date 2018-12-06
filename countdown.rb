d125543aa6b308ddef4292e097869b80448dc8a4
Manny Baez <jesusmbaez@gmail.com>
2018-12-06 22:23:39 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
-end
+def countdown(number)
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end 
+else "HAPPY NEW YEAR!"
+end 
+
+def countdown(number)
+  sleep 5
+  while number > 0
+  puts "#{number} SECOND(S)!"
+  number -= 1
+end 
+else "HAPPY NEW YEAR!"
+end 
